6fe186a57a63ea7de8d999ff6cab381981fdcbdc
Palespyder <jason.lewis@gmail.com>
2022-08-14 19:12:15 -0400

    Got movement working and I am now able to see the sprinte. I had forgotten to inherit from pygame.sprint.Sprite for the snake.

--- Python/Games/NoStepOnSnek/snake.py
+++ Python/Games/NoStepOnSnek/snake.py
@@ -1,37 +1,47 @@
 import pygame
 
-class Snake:
+
+class Snake(pygame.sprite.Sprite):
     def __init__(self, pos, group):
-        self.speed = 10
+        super().__init__(group)
         self.position = pos
         self.group = group
-        self.direction = pygame.math.Vector2((0, 0))
         self.section_size = 32
         self.sections = []
         self.num_sections = 1
-        self.image = pygame.Surface((32, 64))
+        self.image = pygame.Surface((self.section_size, self.section_size))
         self.image.fill('black')
         self.rect = self.image.get_rect(center=pos)
+
+        # Movement
+        self.direction = pygame.math.Vector2()
+        self.speed = 300
 
     def input(self):
         keys = pygame.key.get_pressed()
 
+        # Vertical Directions
         if keys[pygame.K_w]:
-            print("Up")
+            self.direction.x = 0
             self.direction.y = -1
         elif keys[pygame.K_s]:
-            print("down")
+            self.direction.x = 0
             self.direction.y = 1
 
-
-        elif keys[pygame.K_d]:
-            print("right")
+        # Horizontal Directions
+        if keys[pygame.K_d]:
+            self.direction.y = 0
             self.direction.x = 1
         elif keys[pygame.K_a]:
-            print("left")
+            self.direction.y = 0
             self.direction.x = -1
+
+    def move(self, dt):
+        self.position += self.direction * self.speed * dt
+        self.rect.center = self.position
 
     def update(self, dt):
         self.input()
+        self.move(dt)
 
 
