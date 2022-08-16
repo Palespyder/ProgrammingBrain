6559ec6f872fbc8df14490ee1440fbdc62acc11e
Palespyder <jason.lewis@gmail.com>
2022-08-15 20:41:48 -0400

    Added Atlys Game

---
+++ Python/Games/Atlys/settings.py
@@ -0,0 +1,23 @@
+# Constants
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+
+# Overlay Positions
+
+
+
+
+# Landscape Layers
+LAYERS = {
+    'salt water': 0,
+    'fresh water': 1,
+    'sand': 2,
+    'soil': 3,
+    'sand water': 4,
+    'soil water': 5,
+    'rain floor': 6,
+    'ground plant': 7,
+    'main': 8
+}
