5b4b26f48c38db202ba4cc884a00f1894958527e
Palespyder <jason.lewis@gmail.com>
2022-09-11 09:29:33 -0400

    Beginning Go - Hello World - Runes

--- Golang/main.go
+++ Golang/main.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"unicode/utf8"
 )
 
 var pl = fmt.Println
 
 func main() {
-	sV1 := "A Word"
-	replacer := strings.NewReplacer("A", "Another")
-	sV2 := replacer.Replace(sV1)
-	pl(sV2)
-	pl("Length: ", len(sV2))
-	pl("Contains Another: ", strings.Index(sV2, "Another"))
-	pl("o index: ", strings.Index(sV2, "o"))
+	rStr := "abcdefg"
+	pl("Rune Count", utf8.RuneCountInString(rStr))
 
 }
