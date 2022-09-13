75019c2d7ba79b5cd4567ad136713d2b7bcc9be0
Palespyder <jason.lewis@gmail.com>
2022-09-12 20:00:03 -0400

    Variables

--- Golang/main.go
+++ Golang/main.go
@@ -8,9 +8,12 @@ var pl = fmt.Println
 
 func main() {
 
-	var x float64
-	x = 20.0
+	var x float64 = 20.0
+
+	y := 42
 	pl(x)
+	pl(y)
 	fmt.Printf("x is of type %T\n", x)
+	fmt.Printf("y is of type %T\n", y)
 
 }
